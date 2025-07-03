f154b4281a640d33f9d8941aa2e54052458a7893
FrancoFerrandi <franco.ferrandi@gmail.com>
2025-07-02 22:00:26 -0300

    Prueba de conexion

---
+++ src/par_2c124/pkg20250703/ferrandifranco/ClasePrueba.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package par_2c124.pkg20250703.ferrandifranco;
+
+/**
+ *
+ * @author el_fr
+ */
+public class ClasePrueba {
+    
+}
